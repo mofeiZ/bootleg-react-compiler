4c611ddd5dc2b090b1f3bffe679b6c7d9e60ac69
Mofei Zhang <feifei0@meta.com>
2024-05-17 16:38:12 -0700

    Step 4: Understand instructions that write to values

--- packages/demo/src/index.ts
+++ packages/demo/src/index.ts
@@ -21,8 +21,35 @@ function getValuesThatMayChange(func: Map<InstrId, Instruction>): Set<InstrId> {
   return mayChange;
 }
 
+// Creating a new object (directly or by calling a function)
+// creates a writable value.
+// (Note that primitives cannot be modified after being created,
+// and neither can params or hook return results)
+//
+// Note: the result of a hook calls themselves are not writable
+// but... calls or objects that use those values might be
+// So let's be conservative and say that the results of hook
+// calls are writable
+function getWritableValues(func: Map<InstrId, Instruction>): Set<InstrId> {
+  const maybeWritable = new Set<InstrId>();
+  for (const [id, instr] of func) {
+    if (["Object", "Call"].includes(instr.kind)) {
+      maybeWritable.add(id);
+    } else {
+      for (const used of eachValue(instr)) {
+        if (maybeWritable.has(used)) {
+          maybeWritable.add(id);
+        }
+      }
+    }
+  }
+  return maybeWritable;
+}
+
 function analyze(func: Map<InstrId, Instruction>): Map<InstrId, ValueInfo> {
   const result = new Map<InstrId, ValueInfo>();
+  // Step 1: Understand which instructions should be memoized
+  // and their dependencies
   const mayChange = getValuesThatMayChange(func);
   for (const [id, instr] of func) {
     const dependencies = new Set<number>();
@@ -41,6 +68,24 @@ function analyze(func: Map<InstrId, Instruction>): Map<InstrId, ValueInfo> {
       shouldMemo: instr.kind === "Call" || instr.kind === "Object",
       dependencies,
     });
+  }
+  print(func, result);
+
+  // Step 2: Understand writes!
+  // Some instructions might write to (and modify) a value created
+  // by an earlier instruction
+  const writableValues = getWritableValues(func);
+  for (const [id, instr] of func) {
+    if (instr.kind === "Call") {
+      for (const used of eachValue(instr)) {
+        if (writableValues.has(used)) {
+          // (Terrible syntax, this creates a new set if instructions
+          //  doesn't exist, then add this instruction to it)
+          result.get(used)!.instructions ??= new Set();
+          result.get(used)!.instructions!.add(id);
+        }
+      }
+    }
   }
   print(func, result);
   return result;
