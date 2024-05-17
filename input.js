fe15fcaf395943768ec227b553b9b89a2b7a39b4
Mofei Zhang <feifei0@meta.com>
2024-05-17 16:40:16 -0700

    Step 5: Let's test our compiler

--- input.js
+++ input.js
@@ -3,5 +3,8 @@ function MusicPlayer({ user, songs }) {
   const items = Array.from(
     songs
   ).unshift(2, 0, makeSong(user));
+  
+  // Just playing around with my new compiler
+  items.push(MY_FAV_SONG)
   // ...
 }
