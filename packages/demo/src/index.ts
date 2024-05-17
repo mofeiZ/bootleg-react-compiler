c87df25c1f085070d07279e3e13f3495370ef2d1
Mofei Zhang <feifei0@meta.com>
2024-05-17 16:34:12 -0700

    Step 3b: Non naive dependencies: what values might change?

--- packages/demo/src/index.ts
+++ packages/demo/src/index.ts
@@ -1,15 +1,37 @@
 import * as Babel from "@babel/core";
 import { ValueInfo, codegenJS, print, readInstructions } from "./Transform";
 import { InstrId, Instruction, eachValue } from "./LoweredJavaScript";
+import { isHookCall } from "./UnderstandMutability";
+
+function getValuesThatMayChange(func: Map<InstrId, Instruction>): Set<InstrId> {
+  const mayChange = new Set<InstrId>();
+  for (const [id, instr] of func) {
+    if (instr.kind === "Param" || isHookCall(instr, func)) {
+      // Parameters and values returned from hooks might change
+      mayChange.add(id);
+    } else {
+      // As can anything calculated off of them
+      for (const used of eachValue(instr)) {
+        if (mayChange.has(used)) {
+          mayChange.add(id);
+        }
+      }
+    }
+  }
+  return mayChange;
+}
 
 function analyze(func: Map<InstrId, Instruction>): Map<InstrId, ValueInfo> {
   const result = new Map<InstrId, ValueInfo>();
+  const mayChange = getValuesThatMayChange(func);
   for (const [id, instr] of func) {
     const dependencies = new Set<number>();
     // Maybe every value an instruction uses should be counted
     // as a dependency?
     for (const used of eachValue(instr)) {
-      dependencies.add(used);
+      if (mayChange.has(used)) {
+        dependencies.add(used);
+      }
     }
 
     // Only call or object instructions can be expensive or create
